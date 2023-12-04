af364480c5ab4d198231e10a138633c223830b61
Rasel006 <rizurasel.cse@gmail.com>
2023-12-04 19:26:31 +0600

    project two

---
+++ project_2/first_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,'first_app/home.html')
--- project_2/project_2/urls.py
+++ project_2/project_2/urls.py
@@ -1,7 +1,8 @@
-"""project_2 URL Configuration
+"""
+URL configuration for project_2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.Home),
+    path('index/',views.index),
+    
+    path('first_app/',include('first_app.urls')),
 ]
