5427c37e933cad0a743d054b86bc2274fa0434c9
Rasel006 <rizurasel.cse@gmail.com>
2023-12-18 03:21:55 +0600

    practice1 done

---
+++ Django Practice Module -14.5/practice/first_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.home,name='homepage'),
+    path('modelform/',views.modelform,name='modelform'),
+]
---
+++ Django Practice Module -14.5/practice/first_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .forms import ContactForm, MyForm
+
+# Create your views here.
+
+def home(request):
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return render(request, 'home.html', {'form': form})
+    else:
+
+        form = ContactForm()
+    return render(request, 'home.html', {'form': form})
+
+def modelform(request):
+
+    if request.method == 'POST':
+
+        form = MyForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return render(request, 'modelform.html', {'form': form})
+    else:
+        form = MyForm()
+        
+    return render(request, 'modelform.html', {'form': form})
